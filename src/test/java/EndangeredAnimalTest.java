e10ac5d53e510a0ca6a7098efc9d8191bf0a1653
David <kingdice@Atomic>
2022-05-09 00:22:46 +0300

    in the endangered animal test we create tests to check whether we can instantiates correctly,gets the health ,name and whether they instantiates successfully.

--- src/test/java/EndangeredAnimalTest.java
+++ src/test/java/EndangeredAnimalTest.java
@@ -1,5 +1,30 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EndangeredAnimalTest {
+    public EndangeredAnimal setupObject() {
+        return new EndangeredAnimal("Lion", "okay", "newborn");
+    }
+
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
+
+    @Test
+    public void endangeredAnimal_instantiatesCorrectly_true() {
+        assertEquals(true, setupObject() instanceof Animal);
+    }
+    @Test
+    public void getName_endangeredAnimalInstantiatesWithName_Elephant() {
+        assertEquals("Elephant", setupObject().getName());
+    }
+    @Test
+    public void getHealth_endangeredAnimalInstantiatesWithHealth_okay() {
+        assertEquals("okay", setupObject().getHealth());
+    }
+    @Test
+    public void getAge_endangeredAnimalInstantiatesWithAge_newborn() {
+        assertEquals("newborn", setupObject().getAge());
+    }
 
 }
