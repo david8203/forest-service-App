01e1bb657127ad28882634360d0ffa597421f257
David <kingdice@Atomic>
2022-05-08 23:40:12 +0300

    here we create our databaserule connection to delete and alter data

--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -1,2 +1,22 @@
+import org.sql2o.*;
 public class DatabaseRule {
+    @Override
+    protected void before() {
+        //development database
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "dice", "8203");  //Those with linux or windows use two strings for username and password
+    }
+
+    @Override
+    protected void after() {
+        try(Connection con = DB.sql2o.open()) {
+            String deleteAnimalsQuery = "DELETE FROM animals *;";
+            String resetAnimalsId = "ALTER SEQUENCE animals_id_seq RESTART WITH 1;;";
+            String deleteSightingsQuery = "DELETE FROM sightings *;";
+            String resetSightingsId = "ALTER SEQUENCE sightings_sighting_id_seq RESTART WITH 1;";
+            con.createQuery(deleteAnimalsQuery).executeUpdate();
+            con.createQuery(resetAnimalsId).executeUpdate();
+            con.createQuery(deleteSightingsQuery).executeUpdate();
+            con.createQuery(resetSightingsId).executeUpdate();
+        }
+    }
 }
