7bbef6dfddf55e2cf555d2c00202df1a9c38ceae
David <kingdice@Atomic>
2022-05-09 00:00:02 +0300

    we create a constructor in our common animal class and adds logis to save common animal.

--- src/main/java/CommonAnimal.java
+++ src/main/java/CommonAnimal.java
@@ -1,2 +1,34 @@
-public class CommonAnimal {
+import org.sql2o.Connection;
+
+import java.util.List;
+
+public class CommonAnimal extends Animal{
+    public static final boolean DATABASE_ENDANGERED = false;
+
+    public CommonAnimal(String name) {
+        this.name = name;
+        endangered = DATABASE_ENDANGERED;
+    }
+
+
+    public void saveCommonAnimal() {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "INSERT INTO animals (name, endangered) VALUES (:name, :endangered);";
+            this.id = (int) con.createQuery(sql, true)
+                    .addParameter("name", this.name)
+                    .addParameter("endangered", this.endangered)
+                    .executeUpdate()
+                    .getKey();
+
+        }
+    }
+
+    public static List<CommonAnimal> all() {
+        String sql = "SELECT * FROM animals WHERE endangered = false;";
+        try(Connection con = DB.sql2o.open()) {
+            return con.createQuery(sql)
+                    .throwOnMappingFailure(false)
+                    .executeAndFetch(CommonAnimal.class);
+        }
+    }
 }
