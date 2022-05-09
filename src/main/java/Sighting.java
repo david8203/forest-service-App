f10e34e27b7e8508801a26e323520fc82e1bc6f5
David <kingdice@Atomic>
2022-05-09 10:28:26 +0300

    we create our initial constructor and initialize our getters and adds the connection in our sighting main class.

--- src/main/java/Sighting.java
+++ src/main/java/Sighting.java
@@ -1,2 +1,42 @@
+import org.sql2o.Connection;
+
 public class Sighting {
+    private int animal_id;
+    private int sighting_id;
+    private String location;
+    private String ranger_name;
+
+    public Sighting(int animal_id, String location, String ranger_name) {
+        this.animal_id = animal_id;
+        this.location = location;
+        this.ranger_name = ranger_name;
+    }
+
+    public int getAnimalId() {
+        return animal_id;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getRangerName() {
+        return ranger_name;
+    }
+
+    public int getSightingId() {
+        return this.sighting_id;
+    }
+
+    public void save() {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "INSERT INTO sightings (animal_id, location, ranger_name) VALUES (:animal_id, :location, :ranger_name);";
+            this.sighting_id = (int) con.createQuery(sql, true)
+                    .addParameter("animal_id", this.animal_id)
+                    .addParameter("location", this.location)
+                    .addParameter("ranger_name", this.ranger_name)
+                    .executeUpdate()
+                    .getKey();
+        }
+    }
 }
