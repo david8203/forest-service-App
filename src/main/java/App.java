29a0a95d072f374ad8831f181aa60145d4404678
David <kingdice@Atomic>
2022-05-09 12:47:43 +0300

    we add our sighting.hbs file

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,7 @@ import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static spark.Spark.*;
@@ -24,6 +25,24 @@ public class App {
             Map<String, Object> model = new HashMap<>();
             model.put("username", request.session().attribute("username"));
             return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+        post("/login", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+
+            String inputtedUsername = request.queryParams("username");
+            request.session().attribute("username", inputtedUsername);
+            model.put("username", inputtedUsername);
+            response.redirect("/");
+            return null;
+        }, new HandlebarsTemplateEngine());
+
+        get("/sightings", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            List<EndangeredAnimal> allEndangeredAnimals = EndangeredAnimal.all();
+            List<CommonAnimal> allCommonAnimals = CommonAnimal.all();
+            model.put("allEndangeredAnimals", allEndangeredAnimals);
+            model.put("allCommonAnimals", allCommonAnimals);
+            return new ModelAndView(model, "sightings.hbs");
         }, new HandlebarsTemplateEngine());
     }
 }
