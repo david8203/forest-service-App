a2b3adc5b7e76f7baa85a459462bb350dbe44ced
David <kingdice@Atomic>
2022-05-10 21:39:48 +0300

    changes heroku credentials

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,42 +1,9 @@
-
-
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import org.sql2o.Connection;
 import org.sql2o.Sql2o;
-import org.sql2o.Sql2oException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.logging.Logger;
-
 
 public class DB {
-    private static URI dbUri;
-    public static Sql2o sql2o;
 
-    static {
-        Logger logger = Logger.getLogger(String.valueOf(DB.class));
+public static Sql2o sql2o=new Sql2o("jdbc:postgresql://ec2-54-172-175-251.compute-1.amazonaws.com:5432/d5r2q5v0ei9mcl","vmapzudwcyefbr", "e84189d37a185edf25301a3247611b82c0c873934cd4590d5c14301047adb300");
 
-        try {
-            if (System.getenv("DATABASE_URL") == null) {
-                dbUri = new URI("postgres://localhost:5432/wildlife_tracker");
-            } else {
-                dbUri = new URI(System.getenv("DATABASE_URL"));
-            }
-            int port = dbUri.getPort();
-            String host = dbUri.getHost();
-            String path = dbUri.getPath();
-            String username = (dbUri.getUserInfo() == null) ? DatabaseProps.username : dbUri.getUserInfo().split(":")[0];
-            String password = (dbUri.getUserInfo() == null) ? DatabaseProps.password : dbUri.getUserInfo().split(":")[1];
-            sql2o = new Sql2o("jdbc:postgresql://" + host + ":" + port + path, username, password);
-        } catch (URISyntaxException e ) {
-            logger.log(Level.WARNING,"not connected to a database");
-        }
-    }
+
 
 }
-//development database
-//    public static Sql2o sql2o = new Sql2o(
-//            "jdbc:postgresql://localhost:5432/wildlife_tracker",
-//            "dice",
-//            "8203");
